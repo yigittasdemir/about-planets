f66a3a71dc60069c236ab5ad18aef152e11914bb
yigittasdemir <yigittasdemir350@gmail.com>
2022-06-08 19:01:00 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './Main.css';
+import Header from './Components/Header';
+import Planets from './Components/Planets';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Planets/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Planets.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+const Planets = () => {
+  return (
+    <div className='planets-container'>
+      <div className="planets">
+         <div className="card">
+             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/600px-Mercury_in_color_-_Prockter07-edit1.jpg" alt="" />
+             <div className="card-info">
+             <h3>Merkür</h3>
+             <p>Güneş Sistemi'ndeki en küçük ve Güneş'e en yakın gezegen. Yaklaşık 88 Dünya gününe eşit yörünge süresi ile Güneş Sistemi'ndeki diğer gezegenlerden daha hızlıdır. Dünya'dan bakıldığında, kendi yörüngesi etrafında 116 günde hareket ettiği görünür. Bilinen hiç doğal uydusu yoktur. Adını tanrıların habercisi Roma tanrısı Merkür'den alır..</p>
+             <a href="https://tr.wikipedia.org/wiki/Merk%C3%BCr"><button>Tamamını Oku</button></a>
+             </div>
+         </div>
+         <div className="card">
+             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/520px-The_Earth_seen_from_Apollo_17.jpg" alt="" />
+             <div className="card-info">
+             <h3>Dünya</h3>
+             <p>Güneş Sistemi'nde Güneş'e en yakın üçüncü gezegen olup şu an için üzerinde yaşam ve sıvı su barındırdığı bilinen tek astronomik cisimdir. Radyometrik tarihleme ve diğer kanıtlara göre 4,5 milyar yıldan fazla süre önce oluşmuştur. Dünya'nın yer çekimi, uzaydaki diğer nesnelerle, özellikle Güneş'le ve tek doğal uydusu Ay'la etkileşime girer. Dünya'nın Güneş'in etrafındaki yörüngesi, 365,256 güneş gün, yani bir yıldız yılı sürer.</p>
+             <a href="https://tr.wikipedia.org/wiki/D%C3%BCnya"><button>Tamamını Oku</button></a>
+             </div>
+         </div>
+         <div className="card">
+             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/PIA23791-Venus-RealAndEnhancedContrastViews-20200608_%28cropped%29.jpg/600px-PIA23791-Venus-RealAndEnhancedContrastViews-20200608_%28cropped%29.jpg" alt="" />
+             <div className="card-info">
+             <h3>Venüs - Çolpan</h3>
+             <p>Güneş Sisteminde, Güneş'e uzaklık bakımından ikinci sıradaki, sıcaklık bakımından da birinci sıradaki gezegendir. Güneşe uzaklık bakımından ikinci sırada olmasına rağmen en sıcak gezegen olmasının nedeni de atmosferinin gelen güneş ışınlarının dışarı çıkmasına izin vermemesidir. Ayrıca Zühre, Çolpan veya Çoban Yıldızı olarak da bilinir. Bu gezegen adını Eski Roma tanrıçası Venüs (Eski Yunan Mitolojisi'nde Afrodit)'ten almıştır.</p>
+             <a href="https://tr.wikipedia.org/wiki/Ven%C3%BCshttps://tr.wikipedia.org/wiki/Ven%C3%BCs"><button>Tamamını Oku</button></a>
+             </div>
+         </div>
+         <div className="card">
+             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/600px-OSIRIS_Mars_true_color.jpg" alt="" />
+             <div className="card-info">
+            <h3>Mars</h3>
+             <p>Güneş Sistemi'nin Güneş'ten itibaren dördüncü gezegeni. Roma mitolojisindeki savaş tanrısı Mars'a ithafen adlandırılmıştır. Yüzeyindeki yaygın demir oksitten dolayı kızılımsı bir görünüme sahip olduğu için "Kızıl Gezegen" de denir. İnce bir atmosferi olan Mars gerek Ay'daki gibi meteor kraterlerini, gerekse Dünya'daki gibi volkan, vadi, çöl ve kutup bölgelerini içeren çehresiyle bir yerbenzeri gezegendir.</p>
+             <a href="https://tr.wikipedia.org/wiki/Mars"><button>Tamamını Oku</button></a>
+             </div>
+         </div>
+         <div className="card">
+             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg/600px-Jupiter_and_its_shrunken_Great_Red_Spot.jpg" alt="" />
+             <div className="card-info">
+             <h3>Jüpiter - Erendiz</h3>
+             <p>Güneş Sistemi'nin en büyük gezegeni. Güneş'ten uzaklığa göre beşinci sırada yer alır. Adını Roma mitolojisindeki tanrıların en büyüğü olan Jüpiter'den alır. Büyük ölçüde hidrojen ve helyumdan oluşmakta ve gaz devi sınıfına girmektedir.</p>
+             <a href="https://tr.wikipedia.org/wiki/J%C3%BCpiter"><button>Tamamını Oku</button></a>
+             </div>
+         </div>
+         <div className="card">
+             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Saturn_from_Cassini_Orbiter_%282004-10-06%29.jpg/600px-Saturn_from_Cassini_Orbiter_%282004-10-06%29.jpg" alt="" />
+             <div className="card-info">
+             <h3>Satürn - Sekentir - Zühal</h3>
+             <p>Güneş Sisteminin Güneş'e yakınlık sırasına göre 6. gezegenidir. Büyüklük açısından Jüpiter'den sonra ikinci sırada gelir. Adını Yunan mitolojisindeki Kronos'tan alır. Çıplak gözle izlenebilen 5 gezegenden biri (diğerleri, Merkür, Venüs, Mars ve Jüpiter) olarak eski çağlardan beri insanoğlunun dikkatini çekmiştir. Büyük ölçüde hidrojen ve helyumdan oluşmakta ve gaz devleri sınıfına girmektedir.</p>
+             <a href="https://tr.wikipedia.org/wiki/Sat%C3%BCrn"><button>Tamamını Oku</button></a>
+             </div>
+         </div>
+         <div className="card">
+             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Uranus2.jpg/600px-Uranus2.jpg" alt="" />
+             <div className="card-info">
+             <h3>Uranüs</h3>
+             <p>Güneş Sisteminin Güneş'ten uzaklık sıralamasına göre 7. gezegenidir. Çap açısından Jüpiter ve Satürn'den sonra üçüncü, kütle açısındansa bu iki gezegen ve Neptün'ün ardından dördüncü sırada gelir. Adını Yunan mitolojisindeki gökyüzü tanrısı Uranos'tan (Yunanca'da Οὐρανός, Latinceleştirilmiş şekli ile Uranus) alır. 1781 yılında William Herschel tarafından keşfedilmiştir ve Buz devi sınıfına girer.</p>
+             <a href="https://tr.wikipedia.org/wiki/Uran%C3%BCs"><button>Tamamını Oku</button></a>
+             </div>
+         </div>
+         <div className="card">
+             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Neptune_-_Voyager_2_%2829347980845%29_flatten_crop.jpg/450px-Neptune_-_Voyager_2_%2829347980845%29_flatten_crop.jpg" alt="" />
+             <div className="card-info">
+             <h3>Neptün</h3>
+             <p>Güneş Sistemi'nin sekizinci, Güneş'e en uzak ve katı yüzeyi bulunmayan gezegenidir. Gaz gezegenler sınıfında yer alan Neptün, Jüpiter ve Satürn'e kıyasla farklı yapısından ötürü buz devi olarak da sınıflandırılır. Güneş sisteminin Uranüs ile beraber en soğuk iki gezegeninden biridir. Katı yüzeye sahip olmamakla birlikte gezegenin dış katmanı genel olarak hidrojen ve helyumdan oluşur. </p>
+             <a href="https://tr.wikipedia.org/wiki/Nept%C3%BCn"><button>Tamamını Oku</button></a>
+             </div>
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default Planets
